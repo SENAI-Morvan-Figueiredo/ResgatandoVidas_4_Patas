48bc7bf6ed743298134d2999c68adfa49e0b4394
Rai <raicarvalho343@gmail.com>
2025-11-24 14:26:11 -0300

    Tentando criar o superadmin

--- ong/ong/settings.py
+++ ong/ong/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "raicarvalho343@gmail.com"   # email que enviará
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")   # email que enviará
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")   # senha de app do Google
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
