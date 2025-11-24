23fc9350b943493ee76034ef148620f6387f79f4
Rai <raicarvalho343@gmail.com>
2025-11-24 14:28:29 -0300

    Tentando ajustar erros de deploy

--- ong/ong/settings.py
+++ ong/ong/settings.py
@@ -153,3 +153,17 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
