da6c4073e4f7205ac6870e5df10530d7e9d1b651
Rai <raicarvalho343@gmail.com>
2025-11-24 21:20:46 -0300

    Ajustando arquivo

--- ong/ong/settings.py
+++ ong/ong/settings.py
@@ -154,13 +154,13 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Email settings usando SendGrid Web API
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'raicarvalho343@gmail.com'
+# Email via SendGrid API
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+
+# e-mail padrão
+DEFAULT_FROM_EMAIL = "raicarvalho343@gmail.com"
 
 
 # Default primary key field type
